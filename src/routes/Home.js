b25c1869b65329612a1956c8584f4bb1c47ccb9a
shoaib900 <rshoaib376@gmail.com>
2023-10-09 15:05:14 +0500

    map method

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div style={{height:"100vh",width:"100%",backgroundColor:"red"}}>
-        <h1>
-            Home
-        </h1>
-      
-    </div>
-  )
-}
+  const [number, setNumber] = useState(0);
+  const [api, setApi] = useState([]);
 
-export default Home
+  const increment = () => {
+    setNumber(number + 1);
+  };
+  const decrement = () => {
+    setNumber(number - 1);
+  };
+  const reset = () => {
+    setNumber(0);
+  };
+
+  console.log("api", api);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setApi(json));
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <h1>Home</h1>
+
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <button onClick={increment}>increment</button>
+        <p>{number}</p>
+        <button onClick={decrement}>decrement</button>
+      </div>
+      <button onClick={reset}>reset value</button>
+
+      <>
+        {api.map(( item) => 
+           (
+              <li key={item.id}> {item.id} -- {item.title}</li>
+          )
+        )}
+      </>
+    </div>
+  );
+};
+
+export default Home;
